a7615781109c4b36cdff4e882d712190af69e5ba
1770383342 <1770383342@qq.com>
2020-06-12 14:04:25 +0800

    c++

---
+++ hello world/三个数相加.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x = 10;
+    int y = 20;
+    int z = 30;
+    cout << "x,y,z三个数的和为：" << x + y + z << endl;
+
+    return 0;
+}
---
+++ 输入输出/输入输出.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "请输入三个数字" << endl;
+    int x, y, z;
+    cin >> x >> y >> z;
+    cout << "三个数字的和为：" << x + y + z << endl;
+
+    return 0;
+}
