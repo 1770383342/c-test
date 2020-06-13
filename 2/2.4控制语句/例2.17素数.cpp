f8cfdb788563872ec9fdff1e852e0478f7243185
1770383342 <1770383342@qq.com>
2020-06-13 16:42:58 +0800

    注释

--- 2/2.4控制语句/例2.17素数.cpp
+++ 2/2.4控制语句/例2.17素数.cpp
@@ -5,11 +5,11 @@ int main()
 {
     int i = 2, j;
     cout << i << '\n';
-    
+
     for(int i = 2; i <= 100; i++)
     {
         j = 2;
-        while (i%j != 0)
+        while (i%j != 0)            //当判断条件为真时跳出循环
         {
             j++;
             if(j==i)
